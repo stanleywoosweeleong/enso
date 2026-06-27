f8e82e4c5f5ed46c314707f7ff732b9981a3668a
stanleywoosweeleong <130345527+stanleywoosweeleong@users.noreply.github.com>
2026-06-27 15:59:33 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * Bump CACHE_VERSION whenever index.html or this file changes, so users get
  * the new version instead of a stale cached one.
  */
-const CACHE_VERSION = 'enso-v1.4.0';
+const CACHE_VERSION = 'enso-v1.5.1';
 const SHELL = [
   './',
   './index.html',
